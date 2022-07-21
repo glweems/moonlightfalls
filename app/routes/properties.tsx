c9b86b32303550997a7b47610cd43fc1b2cdcbad
garrett <garrett.txt@gmail.com>
2022-07-21 14:31:15 -0500

    style(properties.tsx): update property card styling

--- app/routes/properties.tsx
+++ app/routes/properties.tsx
@@ -1,24 +1,27 @@
 import { MetaFunction } from "@remix-run/node";
 import { string } from "joi";
-import { Fragment } from "react";
+import { FC, Fragment, MouseEventHandler, useState } from "react";
+import { ChevronRight } from "tabler-icons-react";
 
 type ManagedProperty = {
   name: string;
   description: string;
   airbnbUrl?: string;
   guests?: number;
-  bedrooms: number;
-  beds: number;
-  baths: number;
+  bedrooms?: number;
+  beds?: number;
+  baths?: number;
   city: string;
-  active: boolean;
+  active?: boolean;
   thumbnail: string;
   photos: string[];
 };
-const properties: Partial<ManagedProperty>[] = [
+const properties: ManagedProperty[] = [
   {
     name: "Dedads",
-    description: "Rustic Ranch 🏡 home with breath taking 🌅 views",
+    description: `If you’re looking to get away from it all, this is the place to go. It is located in the scenic rolling hills of the North Texas Hill Country surrounded with mature, tall oak trees. Breathtaking views, beautiful sunsets, and stargazing skies create the perfect escape from the city. It’s a hiker’s dream, housing Montague County's highest peaks.
+
+Enjoy the views while cooking the perfect meal with the industrial grade outdoor kitchen followed with smores around the fire pit and a glass of wine. The house is fully equipped with washer, dryer, and fully stocked kitchen. Stay in touch with cell phone boosters, WiFi, Netflix, Roku & DVD players.`,
     airbnbUrl:
       "https://www.airbnb.com/rooms/27871611?source_impression_id=p3_1656687711_u5KS%2F59HwMYP3JRo",
     guests: 13,
@@ -27,6 +30,7 @@ const properties: Partial<ManagedProperty>[] = [
     baths: 3,
     city: "Saint Jo, Texas",
     active: true,
+    thumbnail: "img/dedads/tbn.jpeg",
     photos: [
       "img/dedads/dedads-32.jpeg",
       "img/dedads/dedads-24.jpeg",
@@ -81,7 +85,7 @@ const properties: Partial<ManagedProperty>[] = [
   },
   {
     name: "Alphas",
-    description: "Rustic farmhouse with southern, musical charm",
+    description: `Kick back and relax in this music-themed, stylish space after visiting Saint Jo’s hot spots! Whether you visit the wineries or Red River Station BBQ for live music, Alpha’s Palace is the perfect place for you.`,
     airbnbUrl:
       "https://www.airbnb.com/rooms/660826655277812867?source_impression_id=p3_1657809302_m5wBDLwfkWL6uwek",
     guests: 6,
@@ -89,7 +93,7 @@ const properties: Partial<ManagedProperty>[] = [
     baths: 2,
     city: "Saint Jo, Texas",
     active: true,
-    thumbnail: "alphas/alphas-15.webp",
+    thumbnail: "img/alphas/tbn.jpg",
     photos: [
       "img/alphas/alphas-22.webp",
       "img/alphas/alphas-18.webp",
@@ -126,7 +130,8 @@ const properties: Partial<ManagedProperty>[] = [
   },
   {
     name: "Bulls Den",
-    description: "Quaint, Unique Rustic Barn Sleeps 6",
+    description:
+      "Get away from it all when you stay under the stars in our unique “Bull’s Den” rustic, western adorned cabin. Located just outside the historical town of Saint Jo. Escape the city and enjoy the quiet days & star-filled night skies! Relax in the country the duration of your stay or head into town & enjoy the best burger in North Texas at the Windmill Grill or delicious BBQ and live music at Red River Station. Make a trip to the vineyards to enjoy the hillside views with a glass of wine!",
     airbnbUrl:
       "https://www.airbnb.com/rooms/660744141399123171?source_impression_id=p3_1657809436_ZZqvupNdOeOnKKYa",
     guests: 6,
@@ -134,6 +139,7 @@ const properties: Partial<ManagedProperty>[] = [
     baths: 1,
     city: "Saint Jo, Texas",
     active: true,
+    thumbnail: "img/bulls-den/tbn.webp",
     photos: [
       "img/bulls-den/bullsden-6.jpeg.webp",
       "img/bulls-den/bullsden-0.jpeg.webp",
@@ -153,34 +159,37 @@ const properties: Partial<ManagedProperty>[] = [
   },
   {
     name: "Beehive",
-    description: "Rustic Ranch 🏡 home with breath taking 🌅 views",
+    description: `Reconnect with loved ones in this family-friendly place.
+This comfortable and cozy ranch home is located on 16 acres in mesmerizing North Texas Hill Country.
+While remote, this property is close to historic Muenster and the German-themed restaurants and events. Less than half a mile away, the Blue Ostrich winery is walking distance. Visit during Oktoberfest and see the heritage sites. Roast s’mores on our fire pit area, rest on hammocks or enjoy sunrise and sunsets on the wrap around porch.`,
     airbnbUrl:
       "https://www.airbnb.com/rooms/665938027103469888/photos/1443152485?source_impression_id=p3_1657808536_63NCryvunp7JzPrn",
     guests: 8,
     bedrooms: 4,
     baths: 2.5,
     city: "Muenster, Texas",
+    thumbnail: "img/beehive/tbn.jpeg",
     photos: [
-      "/img/behive/behive-15.jpeg",
-      "img/behive/behive-3.jpeg",
-      "img/behive/behive-18.jpeg",
-      "img/behive/behive-2.jpeg",
-      "img/behive/behive-14.jpeg",
-      "img/behive/behive-9.jpeg",
-      "img/behive/behive-13.jpeg",
-      "img/behive/behive-5.jpeg",
-      "img/behive/behive-4.jpeg",
-      "img/behive/behive-12.jpeg",
-      "img/behive/behive-8.jpeg",
-      "img/behive/behive-7.jpeg",
-      "img/behive/behive-11.jpeg",
-      "img/behive/behive-10.jpeg",
-      "img/behive/behive-6.jpeg",
-      "img/behive/behive-tbn.jpeg",
-      "img/behive/behive-1.jpeg",
-      "img/behive/behive-17.jpeg",
-      "img/behive/behive-16.jpeg",
-      "img/behive/behive-0.jpeg",
+      "/img/beehive/beehive-15.jpeg",
+      "img/beehive/beehive-3.jpeg",
+      "img/beehive/beehive-18.jpeg",
+      "img/beehive/beehive-2.jpeg",
+      "img/beehive/beehive-14.jpeg",
+      "img/beehive/beehive-9.jpeg",
+      "img/beehive/beehive-13.jpeg",
+      "img/beehive/beehive-5.jpeg",
+      "img/beehive/beehive-4.jpeg",
+      "img/beehive/beehive-12.jpeg",
+      "img/beehive/beehive-8.jpeg",
+      "img/beehive/beehive-7.jpeg",
+      "img/beehive/beehive-11.jpeg",
+      "img/beehive/beehive-10.jpeg",
+      "img/beehive/beehive-6.jpeg",
+      "img/beehive/beehive-tbn.jpeg",
+      "img/beehive/beehive-1.jpeg",
+      "img/beehive/beehive-17.jpeg",
+      "img/beehive/beehive-16.jpeg",
+      "img/beehive/beehive-0.jpeg",
     ],
   },
 
@@ -195,52 +204,66 @@ export const meta: MetaFunction = () => {
 };
 export default function PropertiesPage() {
   return (
-    <div className="md:grid-cols-2 gap container-2xl container grid w-full gap-4">
-      {properties.map((p) => {
-        console.log("p: ", p);
-        const subtitle = [
-          `${p.guests} Guests`,
-          `${p.bedrooms} Bedrooms`,
-          `${p.beds} Beds`,
-          `${p.baths} Baths`,
-        ];
-
-        return (
-          <div className="card bg-base-100 card-bordered justify-center rounded-none shadow-sm">
-            <figure className="carousel">
-              {p?.photos?.map((photo) => (
-                <div key={photo} className="carousel-item w-full">
-                  <img
-                    src={photo}
-                    className=" object-cover w-auto"
-                    alt="Tailwind CSS Carousel component"
-                  />
-                </div>
-              ))}
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{p.name}</h2>
-              <p>
-                {subtitle.map((txt, index) => (
-                  <Fragment key={`sub-${index}`}>
-                    {txt}
-                    {index !== subtitle.length - 1 && (
-                      <span className="mx-3 font-bold">·</span>
-                    )}
-                  </Fragment>
-                ))}
-              </p>
-              <p>{p.description}</p>
-              {/* <div className="card-actions justify-end">
-                <div className="badge badge-outline">{p.guests} Guests</div>
-                <div className="badge badge-outline">{p.bedrooms} Bedrooms</div>
-                <div className="badge badge-outline">{p.beds} Beds</div>
-                <div className="badge badge-outline">{p.baths} Baths</div>
-              </div> */}
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <section className="bg-white">
+      <div className="sm:py-8 md:py-12 sm:space-y-8 md:space-y-16 max-w-7xl w-full px-5 py-6 mx-auto space-y-5">
+        <div className="sm:px-5 gap-x-8 gap-y-16 grid grid-cols-12 pb-10">
+          {properties.map((p) => (
+            <Card key={p.name} property={p} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
+
+const Card: FC<{ property: ManagedProperty }> = ({ property: p }) => {
+  const [showMore, setShowMore] = useState(false);
+  const handleClick: MouseEventHandler<HTMLParagraphElement> = (e) => {
+    e.preventDefault();
+    setShowMore(!showMore);
+  };
+
+  return (
+    <div className="sm:col-span-6 xl:col-span-4 flex flex-col items-start w-full col-span-12 space-y-3">
+      <img
+        className="h-50 object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm"
+        src={p.thumbnail}
+      />
+
+      <h2 className="sm:text-xl md:text-2xl text-lg font-bold">
+        <a href="#_">{p.name}</a>
+      </h2>
+
+      <p className="text-xs font-medium">
+        {p.guests && (
+          <span className="mx-1 text-gray-600">{p.guests} guests ·</span>
+        )}
+
+        {p.bedrooms && (
+          <span className="mx-1 text-gray-600">{p.bedrooms} bedrooms ·</span>
+        )}
+
+        {p.beds && <span className="mx-1 text-gray-600">{p.beds} beds ·</span>}
+
+        {p.baths && <span className="mx-1 text-gray-600">{p.baths} baths</span>}
+      </p>
+
+      <p className="text-xs font-medium">
+        <a href="#_" className="mr-1 underline">
+          {p.city}
+        </a>
+      </p>
+
+      <p
+        className="flex flex-col text-sm text-gray-500 cursor-pointer"
+        onClick={handleClick}
+      >
+        {!showMore ? `${p.description.substring(0, 150)}...` : p.description}
+        <div className="hover:underline flex items-center pt-2">
+          <span className="font-medium">Show more</span>
+          <ChevronRight size={15} className="inline-block" />
+        </div>
+      </p>
+    </div>
+  );
+};
